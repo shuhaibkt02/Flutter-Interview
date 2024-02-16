ecb3ceacb2903c01d77336ea0a07390a74bc6f73
shuhaibkt02 <shuhaibkt02@gmail.com>
2024-02-16 16:36:54 +0530

    intermediate task 1 question issue solved

--- lib/presentaion/intermediate/task_1.dart
+++ lib/presentaion/intermediate/task_1.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 /// You’re making a shopping app called RubberBaby, which sells dolls.
-/// Unfortunately, you’ve run into a problem on the order page. 
+/// Unfortunately, you’ve run into a problem on the order page.
 /// If a customer makes one order for blue dolls and another order for red dolls but then tries to delete the blue doll order,
 ///  the red doll order is wrong.
 
@@ -18,15 +18,22 @@ class _OrderPageState extends State<OrderPage> {
   Widget build(BuildContext context) {
     return Column(children: [
       ElevatedButton(
-        child: (Text('Delete blue')),
+        child: (const Text('Delete blue')),
         onPressed: () {
           setState(() {
-            isShowing = false;
+            isShowing = !isShowing;
           });
         },
       ),
-      // if (isShowing) CounterButton(color: Colors.blue),
-      // CounterButton(color: Colors.red),
+      if (isShowing)
+        Container(
+          color: Colors.blue,
+          height: 40,
+        ),
+      Container(
+        color: Colors.red,
+        height: 40,
+      ),
     ]);
   }
 }
